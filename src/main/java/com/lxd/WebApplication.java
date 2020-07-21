0ff3725d35ff43e74aef45d187d2beb61db0ce23
gcc135 <1442458224@qq.com>
2020-07-21 10:48:30 +0800

    调整格式,测试gcc帐号提交git代码

--- src/main/java/com/lxd/WebApplication.java
+++ src/main/java/com/lxd/WebApplication.java
@@ -2,13 +2,16 @@ package com.lxd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @Description: 前端应用启动类
+ * @author gengchengcheng
+ * @date 2020/7/21 10:45
+ */
 @SpringBootApplication
-public class DemoApplication {
+public class WebApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WebApplication.class, args);
 	}
 
 }
